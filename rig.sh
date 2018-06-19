8ee4f8fae39552b8dc44e715df8a4c456bd40b2b
Krishna Bharadwaj <bharadwaj6598@gmail.com>
2018-06-19 14:46:32 +0530

    rig.sh now working as expected

--- rig.sh
+++ rig.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 gcloud compute instances create rig-1 \
-    --custom-cpu 6 --custom-memory 4 --zone us-central1-c \
-    --image-family ubuntu-1604-lts --image-project ubuntu-os-cloud \
-    --metadata-from-file startup-script=startup.sh \
+    --custom-cpu 6 --custom-memory 6 --zone us-central1-c \
+    --image=cos-stable-67-10575-55-0 --image-project=cos-cloud \
+    --metadata=startup-script="docker pull ufoym/deepo:all-py36-jupyter-cpu" \
     --preemptible \
-&& gcloud compute scp --recurse ./ root@rig-1:/root/app \
-&& gcloud compute ssh root@rig-1 -- \
-'docker run -it -v ~/app:/app ufoym/deepo:all-py36-jupyter-cpu python3 /app/$1' \
-&& gcloud -q compute instances delete rig-1
+&& gcloud compute scp --zone us-central1-c --recurse ./ rig-1:~/app \
+&& gcloud compute ssh --zone us-central1-c rig-1 -- \
+"docker run -it -v ~/app:/app ufoym/deepo:all-py36-jupyter-cpu python3 /app/$1" \
+&& gcloud -q compute instances delete rig-1 --zone us-central1-c
